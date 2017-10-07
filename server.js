421496b65e0047211cc86efb69ca293655da0a6e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-07 22:40:39 +0530

    zero degree
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ const express = require('express');
 const keypress = require('keypress');
 const servo = require('pi-servo');
 keypress(process.stdin);
-let sv18 = new servo(18);
+var sv18 = new servo(18);
 process.stdin.on('keypress',function (ch, key) {
     console.log("-------")
     console.log(key.name);
     if(key.name === 'h'){
         sv18.open().then(function(){
-            sv18.setDegree(100); // 0 - 180
+            sv18.setDegree(180); // 0 - 180
         });
+    }
 
+    if(key.name === 'o'){
+        sv18.open().then(function(){
+            sv18.setDegree(0); // 0 - 180
+        });
     }
 
     if (key && key.ctrl && key.name == 'c') {
